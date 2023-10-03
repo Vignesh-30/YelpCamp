acd93106837e898b4ec926198abeeabd5e8beff3
Vignesh-30 <30vignesh@gmail.com>
2023-10-03 12:26:40 +0800

    created a middleware to store flash messages

--- app.js
+++ app.js
@@ -6,18 +6,34 @@ const methodOverride = require("method-override");
 const ExpressError = require("./utilities/ExpressError");
 const app = express();
 const ejsmate = require('ejs-mate');
+const session = require("express-session");
+const flash = require("connect-flash");
 
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
 
+const sessionConfig = {
+    secret: "thisisasecretcode",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 *24 * 7,
+        maxAge: 1000 * 60 * 60 *24 * 7
+    }
+}
+
+
 app.engine('ejs', ejsmate);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use("/campgrounds", campgrounds);
-app.use("/campgrounds/:id/reviews", reviews);
+
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(session(sessionConfig));
+app.use(flash());
 
 mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
@@ -29,12 +45,18 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
         console.log(error);
     });
 
+app.use((req,res,next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+});
 
+app.use("/campgrounds", campgrounds);
+app.use("/campgrounds/:id/reviews", reviews);
 
 app.get("/", (req, res) => {
     res.render("home");
 });
-
 
 app.all("*", (req, res, next) => {
     next(new ExpressError("Page not found", 404))
@@ -45,6 +67,7 @@ app.use((err, req, res, next) => {
     if (!err.message) err.message = 'Oh No, Something Went Wrong!'
     res.status(statusCode).render('error', { err })
 })
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
